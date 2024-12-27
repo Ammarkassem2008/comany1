03b1111db36bd374994075f75919e4321752b235
Ammarkassem2008 <ammarkassem215@gmail.com>
2024-12-27 14:34:47 +0300

    update

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,38 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="style/style.css">
-  <link rel="stylesheet" href="style/Components/header.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-  <title>Buy Your Games Here</title>
-</head>
-
-<body>
-  <!-- Header Start -->
-  <header class="bg-gray-900">
-    <a class="logo" href="#"><img src="images/logo.png"></a>
-
-    <label for="check">
-      <i id="btn-menu" class="fa-solid fa-bars"></i>
-    </label>
-    <input type="checkbox" id="check">
-
-    <nav id="nav-links" class="bg-gray-900">
-      <a href="">Gifts</a>
-      <a href="">Subscribes</a>
-      <a href="">New Games</a>
-      <a href="">Old Games</a>
-    </nav>
-
-  </header>
-  <!-- Header End -->
-
-  <div style="height: 1000px">
-
-  </div>
   <!--start footer -->
   <footer class="bg-gray-900 text-gray-400 py-10">
       <div href="/" class="footer-logo flex justify-center">
