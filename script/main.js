f85799809d3c1f4c0585fbc2d1c97f883f235793
K-salloum <kinansalloum2008@gmail.com>
2024-12-27 20:27:12 +0300

    hg

--- script/main.js
+++ script/main.js
@@ -7,3 +7,10 @@ btnMenu.onclick = function() {
 }
 
 // Image Changer
+
+let image = document.getElementById('image');
+let images = ['./images/img1.jpg' , './images/img2.jpg' , './images/img3.jpg' , './images/img4.jpg' , './images/img5.jpg'];
+setInterval(function(){
+  let rnd = Math.floor(Math.random()*images.length);
+  image.src = images[rnd];
+},5000);
